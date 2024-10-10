fb9def87f6b6b12d778e1a61df89efe6ed7f32a8
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-10-10 11:52:42 -0300

    Agregada routes de registrar usuario, modulo users y views

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
+    children: [
+      {
+        path: 'post',
+        title: 'Registrar usuario',
+        loadComponent: () =>
+          import('./dashboard/views/users/post/post.component').then(
+            (m) => m.PostComponent
+          ),
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+];
