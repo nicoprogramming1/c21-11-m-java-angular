cbbd0d64be99d6fb95fb8582b452f257cebd48e4
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-10-10 22:22:09 -0300

    Cambios en la estructura

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,31 +1,5 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'dashboard',
-    loadComponent: () =>
-      import('./dashboard/dashboard.component').then(
-        (m) => m.DashboardComponent
-      ),
-    children: [
-      {
-        path: 'user',
-        title: 'Consultar usuario',
-        loadComponent: () =>
-          import('./dashboard/views/usuarios/usuario/usuario.component').then(
-            (m) => m.UsuarioComponent
-          ),
-      },
-      {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full',
-      },
-    ],
-  },
-  {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
-  },
+  
 ];
