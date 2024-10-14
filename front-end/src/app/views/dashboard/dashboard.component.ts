06da3468c1c5d902e13ec27feeba59e157b692d5
dewinson1 <dewinson12@gmail.com>
2024-10-14 09:36:09 -0400

    v2

--- front-end/src/app/views/dashboard/dashboard.component.ts
+++ front-end/src/app/views/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HEADComponent } from '../head/head.component';
+import {AsideComponent} from '../aside/aside.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HEADComponent],
+  imports: [HEADComponent,AsideComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
