e5ab96b04b8b74fac606250cd9fd45afd03ecbed
candela <gcandela894@gmail.com>
2024-10-15 14:19:21 -0300

    Vuelvo a añadir save-user y dejo la ruta descomentada

---
+++ front-end/src/app/views/users/save-user/save-user.component.ts
@@ -0,0 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-save-user',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './save-user.component.html',
+  styleUrl: './save-user.component.css'
+})
+export class SaveUserComponent {
+  private userService = inject(UserService);
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      birthday: ['', Validators.required],
+      dni: ['', [Validators.required, Validators.minLength(10)]],
+      role: ['', Validators.required],
+      legajo: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      const user: User = {
+        dni: this.registerForm.value.dni,
+        email: this.registerForm.value.email,
+        role: this.registerForm.value.rol,
+        legajo: this.registerForm.value.legajo,
+        lastName: this.registerForm.value.lastName,
+        firstName: this.registerForm.value.firstName,
+        birthDay: this.registerForm.value.birthDay,
+      }
+
+      this.userService.saveUser(user).subscribe(
+        response => {
+          console.log('User registered successfully', response);
+        },
+        error => {
+          console.error('Error registering user', error);
+        }
+      );
+    }
+  }
+
+  resetForm(): void {
+    this.registerForm.reset();
+  }
+}
