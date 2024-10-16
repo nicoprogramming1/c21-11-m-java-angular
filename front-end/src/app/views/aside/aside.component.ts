fef32ab0e5faae89edff698653c2652789edebd0
dewinson1 <dewinson12@gmail.com>
2024-10-16 00:14:19 -0400

    v9

--- front-end/src/app/views/aside/aside.component.ts
+++ front-end/src/app/views/aside/aside.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ListUserComponent } from '../list-user/list-user.component';
 import { routes } from '../../app.routes';
+import { SaveUserComponent } from '../users/save-user/save-user.component';
 
 
 @Component({
   selector: 'app-aside',
   standalone: true,
-  imports: [ListUserComponent],
+  imports: [ListUserComponent, SaveUserComponent],
   templateUrl: './aside.component.html',
   styleUrl: './aside.component.css'
 })
 export class AsideComponent {
+
+  currentView = signal<'list' | 'register'>('list');
+
+  setView(view: 'list' | 'register', event: Event) {
+    event.preventDefault(); // Previene la recarga de la página
+    this.currentView.set(view);
+  }
 
 }
